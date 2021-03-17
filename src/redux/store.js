6e3a31276b592bf7f99c334245eff3d08a8e1fc7
saksaikot <saksaikot@gmail.com>
2021-03-17 20:14:05 +0600

    apply redux-middleware logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import { reducer } from "./reducer";
 
-export const myStore = createStore(reducer);
+export const myStore = createStore(reducer, applyMiddleware(logger));
